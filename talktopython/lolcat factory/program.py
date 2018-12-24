7457b795dcbb0f3c4423e8a21c0cdff87fdf7810
metalGod <metalaarif@gmail.com>
2018-12-24 18:50:59 +0000

    half way to completing

--- talktopython/lolcat factory/program.py
+++ talktopython/lolcat factory/program.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+import os
+
+
+def main():
+    header()
+    create_folder()
+
+
+def header():
+    print("-" * 50)
+    print("\t\t\t LoL Cat Factory")
+    print("-" * 50)
+
+def create_folder():
+    current_path = os.path.dirname(__file__)
+    folder_name = "cat_pics"
+    photo_path = os.path.join(current_path, folder_name)
+
+    if not os.path.exists(photo_path) or not os.path.isdir(photo_path):
+        print("Creating a folder.....{}".format(photo_path))
+        os.mkdir(photo_path)
+
+    return photo_path
+
+if __name__ == '__main__':
+    main()
